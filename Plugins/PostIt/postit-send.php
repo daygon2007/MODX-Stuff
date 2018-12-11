1462b7069da3150e4d4214fdf8002b0ca9a323dd
Harrelson <jharrelson@alertlogic.com>
2018-12-11 12:11:29 -0600

    Adding New Plugin

--- Plugins/PostIt/postit-send.php
+++ Plugins/PostIt/postit-send.php
@@ -1,12 +1,13 @@
 <?php
 if ( $modx->event->name == 'OnDocFormSave' ) {
-	$page_id = $resource->get( 'id' );
-	$page_desc = $resource->get( 'description' );
-	$page_content = $resource->get( 'content' );
-	$page_image = $resource->getTVValue( 'page_image' );
-	$page_amp = $resource->getTVValue( 'amp_template' );
-	$page_featured = $resource->getTVValue( 'featured' );
-	$page_uri = $resource->get( 'uri' );
+	$page_title = $resource->get( 'pagetitle' ); // Title of the resource
+	$page_id = $resource->get( 'id' ); // ID of the resource
+	$page_desc = $resource->get( 'description' ); // The description field of the resource
+	$page_content = $resource->get( 'content' ); // The content body of the resource
+	$page_image = $resource->getTVValue( 'page_image' ); // The page image Template Variable
+	$page_amp = $resource->getTVValue( 'amp_template' ); // The AMP Template
+	$page_featured = $resource->getTVValue( 'featured' ); // The featured template variable
+	$page_uri = $resource->get( 'uri' ); // The URI of the resource
 
 	if ( $mode == 'new' ) {
 		// resource created
@@ -15,6 +16,9 @@ if ( $modx->event->name == 'OnDocFormSave' ) {
 		// existing resource was updated
 		return 
 			'New ID: ' . $page_id .
+			'
+			'.
+			'Page Title: ' . $page_title .
 			'
 			'.
 		 	' New Description: ' . $page_desc .
